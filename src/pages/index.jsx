ea02daae632b179f19f75a777db7eb5b37698ec7
Joaovitorufu <joaovitorufu@gmail.com>
2021-06-29 15:17:26 -0300

    reder do document, story book e adicionando components

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,10 @@
-import Head from 'next/head'
-import styled from 'styled-components'
+import Head from 'next/head';
+import styled from 'styled-components';
 
 const Heading = styled.h1`
-  background: ${({theme}) => theme.colors.secondaryColor};
-
+  background: ${({ theme }) => theme.colors.secondaryColor};
 `;
 
 export default function Home() {
-  return (
-  <Heading>Oi</Heading>
-  )
+  return <Heading>Oi</Heading>;
 }
