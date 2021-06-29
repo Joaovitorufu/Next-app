96bf5705c83ca49cdeae1d472186b44379d12029
Joaovitorufu <joaovitorufu@gmail.com>
2021-06-29 11:51:19 -0300

    Configurando o styled-components

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import {ThemeProvider} from 'styled-components'
+import { theme } from "../styles/theme"
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme = {theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+
+  )
 }
 
 export default MyApp
